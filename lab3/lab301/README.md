5b06ecc0641c3300897eac4bdd6d832e39827e8f
daniff15 <dani.fig@ua.pt>
2021-11-11 12:19:31 +0000

    lab301 a) c) completed

---
+++ lab3/lab301/README.md
@@ -0,0 +1,13 @@
+## PERGUNTAS ALINEA B
+
+# The “UserController” class gets an instance of “userRepository” through its constructor; how is this new repository instantiated? 
+
+
+# List the methods invoked in the “userRepository” object by the “UserController”. Where are these methods defined?
+
+
+# Where is the databeing saved?
+
+Os dados estão a ser guardados com o H2 database, ou seja, estão a ser guardados em memória, o que significa que não é persistente.
+
+# Where is the rule for the “not empty” email address defined?
---
+++ lab3/lab301/demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @Pattern(regexp="(^$|[0-9]{9})")
+    @NotBlank(message = "Phone number is mandatory")
+    private String phoneNumber;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", email=" + email + ", phoneNumber=" + phoneNumber + '}';
+    }
+}
